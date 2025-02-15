8b7de4994c0cff2c60405449817a0e91023d32de
Ali Roohan <aliroohan321@gmail.com>
2025-02-15 15:00:20 +0500

    Resolved routing issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,9 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'machine/:basic/:single-head',
+    renderMode: RenderMode.Prerender
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
