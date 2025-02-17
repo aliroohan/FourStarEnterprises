f444c7fbb206a66c0243b56463f280e2dce4c1ee
Ali Roohan <aliroohan321@gmail.com>
2025-02-17 10:41:29 +0500

    Resolved routing issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServerRouting } from '@angular/ssr';
+import { serverRoutes } from './app.routes.server';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideRouter(routes) , provideServerRouting(serverRoutes), provideClientHydration(withEventReplay()), provideHttpClient()],
 };
---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,13 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+export const serverRoutes: ServerRoute[] = [
+  
+
+  {
+    path: 'machine/:basic/:single-head',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
+];
