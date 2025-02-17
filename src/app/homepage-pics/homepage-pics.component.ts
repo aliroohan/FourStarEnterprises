18e0ff1825d49eed5dd3cc498cc303de9682d824
Ali Roohan <aliroohan321@gmail.com>
2025-02-17 21:40:48 +0500

    Removed slider

--- src/app/homepage-pics/homepage-pics.component.ts
+++ src/app/homepage-pics/homepage-pics.component.ts
@@ -10,10 +10,6 @@ export class HomepagePicsComponent implements OnInit {
   currentIndex: number = 0; 
 
   ngOnInit() {
-    setInterval(() => {
-      this.currentIndex = (this.currentIndex + 1) % 3;
-      this.updateSlides(); 
-    }, 5000);
   }
 
   currentSlide(index: number) {
