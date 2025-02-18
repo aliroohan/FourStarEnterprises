7103bfed304d3789cf87e9bb32b16dba09f80ef5
Ali Roohan <aliroohan321@gmail.com>
2025-02-18 20:40:59 +0500

    Added play/pause button in video

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -7,8 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  play = false;
   addPlay() {
     const video = document.querySelector('video');
+    const play = document.querySelector('.pla img');
+    
+    if (!this.play) {
+    play?.setAttribute('src', '/assets/pause.png');
     video?.play();
+    this.play = true;
+    } else {
+    play?.setAttribute('src', '/assets/play_btn.png');
+    video?.pause();
+    this.play = false;
+    }
   }
 }
